11541e0efaa7d06e593412f2969166d1a3462217
a-ma-n <amanali.siddiqui2020@vitstudent.ac.in>
2022-07-29 20:59:50 +0530

    text fixes

--- src/portfolio.js
+++ src/portfolio.js
@@ -500,7 +500,7 @@ const experience = {
 const projectsHeader = {
   title: "Projects",
   description:
-    "My projects makes use of vast variety of latest technology tools. My best experience is to create projects with React and deploy them to web applications using cloud infrastructure.",
+    "My projects make use of vast variety of latest technology tools. My best experience is to create projects with React and deploy them to web applications using cloud infrastructure.",
   avatar_image_path: "projects_image.svg",
 };
 
